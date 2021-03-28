bc16e0dce74e52bebcf6b97f121e078b6f036c29
cswaney <colinswaney@gmail.com>
2021-03-28 15:38:00 -0400

    Add getTodos function
    - Create getTodos.ts
    - Add GetTodos to Serverless configuration

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,49 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+
+const client = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // TODO: Get all TODO items for a current user
+    // const userId = JSON.parse(event.body).userId
+    console.log('event:', event)
+    const userId = '55fa3605-2082-484a-bd71-4d9ff9fcd8af'
+
+    // Query todos
+    const result = await client.query({
+        TableName: todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+            ':userId': userId
+        }
+    }).promise()
+    const todos = result.Items
+
+    // Check if user exists
+    const userExists = true
+
+    // Return response
+    if (userExists) {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                todos
+            })
+        }
+    } else {
+        return {
+            statusCode: 404,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                error: 'User does not exist'
+            })
+        }
+    }
+}
